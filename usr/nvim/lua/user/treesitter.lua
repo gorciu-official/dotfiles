8ec5dd4d8b0c629c281df912a1453fcca8110e4b
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2026-01-26 18:39:59 +0100

    something is coming

--- usr/nvim/lua/user/treesitter.lua
+++
@@ -1,7 +0,0 @@
--- gpt wrote it too
-
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "typescript", "javascript", "rust", "python", "lua" },
-  highlight = { enable = true },
-  indent = { enable = true },
-}
